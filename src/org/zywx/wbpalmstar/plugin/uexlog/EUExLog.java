3bf00d57994b5d2df0a3a4cecc8cc31d3f9e63ee
yltwust <510212753@qq.com>
2016-08-30 10:52:56 +0800

    引擎改为远程依赖,格式化代码

--- src/org/zywx/wbpalmstar/plugin/uexlog/EUExLog.java
+++ src/org/zywx/wbpalmstar/plugin/uexlog/EUExLog.java
@@ -2,7 +2,6 @@ package org.zywx.wbpalmstar.plugin.uexlog;
 
 import android.content.Context;
 import android.widget.Toast;
-
 import org.zywx.wbpalmstar.base.ResoureFinder;
 import org.zywx.wbpalmstar.engine.EBrowserView;
 import org.zywx.wbpalmstar.engine.universalex.EUExBase;
@@ -12,126 +11,122 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
-import java.net.SocketException;
-import java.net.UnknownHostException;
+import java.net.*;
 
 public class EUExLog extends EUExBase {
 
-	public static final String tag = "uexLog_";
+    public static final String tag = "uexLog_";
 
-	public static final String F_FILE_NAME = "widgetOne.log";
+    public static final String F_FILE_NAME = "widgetOne.log";
 
-	private static final int logServerPort = 30050;
+    private static final int logServerPort = 30050;
 
-	private String m_logServerIp;
+    private String m_logServerIp;
 
-	private DatagramSocket m_udp;
+    private DatagramSocket m_udp;
 
-	private Context m_context;
+    private Context m_context;
 
-	public EUExLog(Context context, EBrowserView inParent) {
-		super(context, inParent);
-		m_context = context;
-	}
+    public EUExLog(Context context, EBrowserView inParent) {
+        super(context, inParent);
+        m_context = context;
+    }
 
-	public void writeLog(String[] parm) {
-		String inLog = parm[0];
-		String widgetPath = null;
-		if (mBrwView.getCurrentWidget() != null) {
-			widgetPath = mBrwView.getCurrentWidget().m_widgetPath;
-		}else{
-            widgetPath= WDataManager.sRootWgt.m_widgetPath;
+    public void writeLog(String[] parm) {
+        String inLog = parm[0];
+        String widgetPath = null;
+        if (mBrwView.getCurrentWidget() != null) {
+            widgetPath = mBrwView.getCurrentWidget().m_widgetPath;
+        } else {
+            widgetPath = WDataManager.sRootWgt.m_widgetPath;
         }
-		if (widgetPath != null) {
-			File file = new File(widgetPath + F_FILE_NAME);
-			if (!file.exists()) {
-				try {
-					file.createNewFile();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-			BufferedWriter bWriter = null;
-			try {
-				bWriter = new BufferedWriter(new FileWriter(file, true));
-				bWriter.write(inLog + "\n");
-				bWriter.flush();
-			} catch (IOException e) {
-				e.printStackTrace();
-			} finally {
-				if (bWriter != null) {
-					try {
-						bWriter.close();
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
-				}
-				bWriter = null;
-			}
-		}
-	}
-
-	private void createUDP() {
-		try {
-			if (m_udp == null) {
-				m_udp = new DatagramSocket();
-				m_udp.setBroadcast(true);
-                if (mBrwView.getCurrentWidget()!=null) {
-                    m_logServerIp = mBrwView.getCurrentWidget().m_logServerIp;
-                }else{
-                    m_logServerIp=WDataManager.sRootWgt.m_logServerIp;
+        if (widgetPath != null) {
+            File file = new File(widgetPath + F_FILE_NAME);
+            if (!file.exists()) {
+                try {
+                    file.createNewFile();
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
-			}
-		} catch (NumberFormatException e) {
-			e.printStackTrace();
-		} catch (SocketException e) {
-			e.printStackTrace();
-		}
-	}
+            }
+            BufferedWriter bWriter = null;
+            try {
+                bWriter = new BufferedWriter(new FileWriter(file, true));
+                bWriter.write(inLog + "\n");
+                bWriter.flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (bWriter != null) {
+                    try {
+                        bWriter.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                bWriter = null;
+            }
+        }
+    }
 
-	public void sendLog(String[] parm) {
-		String inLog = parm[0];
-		createUDP();
-		if (m_udp == null || inLog == null || inLog.length() == 0) {
-			return;
-		}
+    private void createUDP() {
+        try {
+            if (m_udp == null) {
+                m_udp = new DatagramSocket();
+                m_udp.setBroadcast(true);
+                if (mBrwView.getCurrentWidget() != null) {
+                    m_logServerIp = mBrwView.getCurrentWidget().m_logServerIp;
+                } else {
+                    m_logServerIp = WDataManager.sRootWgt.m_logServerIp;
+                }
+            }
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+    }
 
-		byte[] data = inLog.getBytes();
-		InetAddress inetAddress;
-		try {
-			inetAddress = InetAddress.getByName(m_logServerIp);
-			DatagramPacket sendPacket = new DatagramPacket(data, data.length,
-					inetAddress, logServerPort);
-			m_udp.send(sendPacket);
-		} catch (UnknownHostException e) {
-			closeUDP();
-			e.printStackTrace();
-		} catch (IOException e) {
-			closeUDP();
-			e.printStackTrace();
-		} catch (SecurityException e) {
-			Toast.makeText(
-					m_context,
-					ResoureFinder.getInstance().getString(mContext,
-							"plugin_log_error_no_permisson_INTERNET"),
-					Toast.LENGTH_SHORT).show();
-		}
+    public void sendLog(String[] parm) {
+        String inLog = parm[0];
+        createUDP();
+        if (m_udp == null || inLog == null || inLog.length() == 0) {
+            return;
+        }
 
-	}
+        byte[] data = inLog.getBytes();
+        InetAddress inetAddress;
+        try {
+            inetAddress = InetAddress.getByName(m_logServerIp);
+            DatagramPacket sendPacket = new DatagramPacket(data, data.length,
+                    inetAddress, logServerPort);
+            m_udp.send(sendPacket);
+        } catch (UnknownHostException e) {
+            closeUDP();
+            e.printStackTrace();
+        } catch (IOException e) {
+            closeUDP();
+            e.printStackTrace();
+        } catch (SecurityException e) {
+            Toast.makeText(
+                    m_context,
+                    ResoureFinder.getInstance().getString(mContext,
+                            "plugin_log_error_no_permisson_INTERNET"),
+                    Toast.LENGTH_SHORT).show();
+        }
 
-	private void closeUDP() {
-		if (m_udp != null) {
-			m_udp.close();
-			m_udp = null;
-		}
-	}
+    }
 
-	@Override
-	protected boolean clean() {
-		closeUDP();
-		return true;
-	}
+    private void closeUDP() {
+        if (m_udp != null) {
+            m_udp.close();
+            m_udp = null;
+        }
+    }
+
+    @Override
+    protected boolean clean() {
+        closeUDP();
+        return true;
+    }
 }
